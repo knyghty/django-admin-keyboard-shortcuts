67fd6c7f6fa87db8a66e0f3dbae924b719c890ee
Tom Carrick <tom@carrick.eu>
2024-06-02 13:54:18 +0200

    Add go to index shortcut

--- django_admin_keyboard_shortcuts/static/admin/js/shortcuts.js
+++ django_admin_keyboard_shortcuts/static/admin/js/shortcuts.js
@@ -1,8 +1,20 @@
+let pressedKeys = new Set();
+
 function isApple() {
   return (
     navigator.platform.indexOf("Mac") === 0 ||
     navigator.platform === "iPhone"
   )
+}
+
+function removePressedKey(key) {
+  pressedKeys.delete(key);
+}
+
+function storeKeyCombo(key) {
+  pressedKeys.add(key);
+  console.log(pressedKeys);
+  setTimeout(function() { removePressedKey(key); }, 5000);
 }
 
 function showDialog() {
@@ -18,6 +30,12 @@ function showDialogOnClick() {
 function handleKeyUp(event) {
   if (event.key === "?") {
     showDialog();
+  } else if (event.key === "g") {
+    console.log(event.key)
+    storeKeyCombo("g");
+  } else if (event.key === "i" && pressedKeys.has("g")) {
+    console.log(event.key);
+    document.location.href = "/admin/";
   }
 }
 
