7cb75517bf47bc5841a0be37767eb644a8714740
erosselli <67162025+erosselli@users.noreply.github.com>
2024-06-08 06:56:03 -0300

    Added input to filter model list in dialog (#14)

--- django_admin_keyboard_shortcuts/static/admin/js/shortcuts.js
+++ django_admin_keyboard_shortcuts/static/admin/js/shortcuts.js
@@ -13,7 +13,6 @@ function removePressedKey(key) {
 
 function storeKeyCombo(key) {
   pressedKeys.add(key);
-  console.log(pressedKeys);
   setTimeout(function() { removePressedKey(key); }, 5000);
 }
 
@@ -51,11 +50,56 @@ function replaceModifiers() {
   }
 }
 
+// This overlaps a lot with the initSidebarQuickFilter function
+// defined in django/contrib/admin/static/admin/js/nav_sidebar.js
+// If/when merged into core, we could try to reuse some parts
+function filterModelList() {
+  const options = [];
+  const modelListDialog = document.getElementById('model-list-dialog');
+  if (!modelListDialog) {
+      return;
+  }
+  modelListDialog.querySelectorAll('li a').forEach((container) => {
+      options.push({title: container.innerHTML, node: container});
+  });
+
+
+  function checkValue(event) {
+      let filterValue = event.target.value;
+      if (filterValue) {
+          filterValue = filterValue.toLowerCase();
+      }
+      if (event.key === 'Escape') {
+          filterValue = '';
+          event.target.value = ''; // clear input
+      }
+      for (const option of options) {
+          let displayValue = '';
+          if (!filterValue || option.title.toLowerCase().indexOf(filterValue) === -1) {
+            displayValue = 'none'
+          }
+          option.node.parentNode.style.display = displayValue;
+      }
+
+  }
+
+  const nav = document.getElementById('model-list-dialog-search');
+  nav.addEventListener('change', checkValue, false);
+  nav.addEventListener('input', checkValue, false);
+  nav.addEventListener('keyup', checkValue, false);
+
+  // We don't want to show anything on the list until the user starts typing
+  checkValue({ target: { value: ''} , key: ''})
+}
+
+
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", showDialogOnClick);
   document.addEventListener("DOMContentLoaded", replaceModifiers);
+  document.addEventListener("DOMContentLoaded", filterModelList);
 } else {
   showDialogOnClick();
   replaceModifiers();
+  filterModelList();
 }
 document.addEventListener("keyup", handleKeyUp);
