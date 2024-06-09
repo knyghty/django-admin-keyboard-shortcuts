2a6479b74dd4f80b626b23feb212ef870b4cba8d
Oliver <oliver@sensibly.nl>
2024-06-09 15:29:56 +0200

    Use block to prevent global variables, add use strict (#27)

--- django_admin_keyboard_shortcuts/static/admin/js/shortcuts_changelist.js
+++ django_admin_keyboard_shortcuts/static/admin/js/shortcuts_changelist.js
@@ -1,65 +1,68 @@
-let checkboxes = null;
-let currentCheckbox = null;
+'use strict';
+{
+  let checkboxes = null;
+  let currentCheckbox = null;
 
-function setUpShortcuts() {
-  checkboxes = document.querySelectorAll("#action-toggle, .action-select");
-}
-
-function focusPreviousCheckbox() {
-  if (!checkboxes.length) {
-    return;
+  function setUpShortcuts() {
+    checkboxes = document.querySelectorAll("#action-toggle, .action-select");
   }
-  if (!currentCheckbox) {
-    currentCheckbox = checkboxes.length - 1;
-  } else {
-    currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) - 1];
-  }
-  currentCheckbox.focus();
-}
 
-function focusNextCheckbox() {
-  if (!checkboxes.length) {
-    return;
+  function focusPreviousCheckbox() {
+    if (!checkboxes.length) {
+      return;
+    }
+    if (!currentCheckbox) {
+      currentCheckbox = checkboxes.length - 1;
+    } else {
+      currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) - 1];
+    }
+    currentCheckbox.focus();
   }
-  if (!currentCheckbox) {
-    currentCheckbox = checkboxes[0];
-  } else {
-    currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) + 1];
+
+  function focusNextCheckbox() {
+    if (!checkboxes.length) {
+      return;
+    }
+    if (!currentCheckbox) {
+      currentCheckbox = checkboxes[0];
+    } else {
+      currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) + 1];
+    }
+    currentCheckbox.focus();
   }
-  currentCheckbox.focus();
-}
 
-function selectCheckbox() {
-  if (currentCheckbox) {
-    currentCheckbox.click();
+  function selectCheckbox() {
+    if (currentCheckbox) {
+      currentCheckbox.click();
+    }
   }
-}
 
-function selectActionsSelect() {
-  const actionsSelect = document.querySelector("select[name=action]")
-  actionsSelect.focus();
-}
+  function selectActionsSelect() {
+    const actionsSelect = document.querySelector("select[name=action]")
+    actionsSelect.focus();
+  }
 
-function handleKeyUp(event) {
-  switch (event.code) {
-    case "KeyK":
-      focusPreviousCheckbox();
-      break;
-    case "KeyJ":
-      focusNextCheckbox();
-      break;
-    case "KeyX":
-      selectCheckbox();
-      break;
+  function handleKeyUp(event) {
+    switch (event.code) {
+      case "KeyK":
+        focusPreviousCheckbox();
+        break;
+      case "KeyJ":
+        focusNextCheckbox();
+        break;
+      case "KeyX":
+        selectCheckbox();
+        break;
       case "KeyA":
         selectActionsSelect();
-      break;
+        break;
+    }
   }
-}
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", setUpShortcuts);
-} else {
-  setUpShortcuts();
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", setUpShortcuts);
+  } else {
+    setUpShortcuts();
+  }
+  document.addEventListener("keyup", handleKeyUp);
 }
-document.addEventListener("keyup", handleKeyUp);
