af5a152a0ee2d1db823603344990230b6c82219c
erosselli <67162025+erosselli@users.noreply.github.com>
2024-06-10 15:10:03 -0300

    Fixes #15 - Adds ESLint

--- django_admin_keyboard_shortcuts/static/admin/js/shortcuts_changelist.js
+++ django_admin_keyboard_shortcuts/static/admin/js/shortcuts_changelist.js
@@ -1,68 +1,68 @@
 'use strict';
 {
-  let checkboxes = null;
-  let currentCheckbox = null;
+    let checkboxes = null;
+    let currentCheckbox = null;
 
-  function setUpShortcuts() {
-    checkboxes = document.querySelectorAll("#action-toggle, .action-select");
-  }
-
-  function focusPreviousCheckbox() {
-    if (!checkboxes.length) {
-      return;
+    function setUpShortcuts() {
+        checkboxes = document.querySelectorAll("#action-toggle, .action-select");
     }
-    if (!currentCheckbox) {
-      currentCheckbox = checkboxes.length - 1;
+
+    function focusPreviousCheckbox() {
+        if (!checkboxes.length) {
+            return;
+        }
+        if (!currentCheckbox) {
+            currentCheckbox = checkboxes.length - 1;
+        } else {
+            currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) - 1];
+        }
+        currentCheckbox.focus();
+    }
+
+    function focusNextCheckbox() {
+        if (!checkboxes.length) {
+            return;
+        }
+        if (!currentCheckbox) {
+            currentCheckbox = checkboxes[0];
+        } else {
+            currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) + 1];
+        }
+        currentCheckbox.focus();
+    }
+
+    function selectCheckbox() {
+        if (currentCheckbox) {
+            currentCheckbox.click();
+        }
+    }
+
+    function selectActionsSelect() {
+        const actionsSelect = document.querySelector("select[name=action]");
+        actionsSelect.focus();
+    }
+
+    function handleKeyUp(event) {
+        switch (event.code) {
+        case "KeyK":
+            focusPreviousCheckbox();
+            break;
+        case "KeyJ":
+            focusNextCheckbox();
+            break;
+        case "KeyX":
+            selectCheckbox();
+            break;
+        case "KeyA":
+            selectActionsSelect();
+            break;
+        }
+    }
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", setUpShortcuts);
     } else {
-      currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) - 1];
+        setUpShortcuts();
     }
-    currentCheckbox.focus();
-  }
-
-  function focusNextCheckbox() {
-    if (!checkboxes.length) {
-      return;
-    }
-    if (!currentCheckbox) {
-      currentCheckbox = checkboxes[0];
-    } else {
-      currentCheckbox = checkboxes[Array.prototype.indexOf.call(checkboxes, currentCheckbox) + 1];
-    }
-    currentCheckbox.focus();
-  }
-
-  function selectCheckbox() {
-    if (currentCheckbox) {
-      currentCheckbox.click();
-    }
-  }
-
-  function selectActionsSelect() {
-    const actionsSelect = document.querySelector("select[name=action]")
-    actionsSelect.focus();
-  }
-
-  function handleKeyUp(event) {
-    switch (event.code) {
-      case "KeyK":
-        focusPreviousCheckbox();
-        break;
-      case "KeyJ":
-        focusNextCheckbox();
-        break;
-      case "KeyX":
-        selectCheckbox();
-        break;
-      case "KeyA":
-        selectActionsSelect();
-        break;
-    }
-  }
-
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", setUpShortcuts);
-  } else {
-    setUpShortcuts();
-  }
-  document.addEventListener("keyup", handleKeyUp);
+    document.addEventListener("keyup", handleKeyUp);
 }
