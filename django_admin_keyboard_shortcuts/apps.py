fd4870dd8c832771102df1e465c0fa3a1c6973b6
khanxmetu <khanxbahria@gmail.com>
2025-03-28 19:27:13 +0000

    Create custom AdminSite, and override default admin site with it

--- django_admin_keyboard_shortcuts/apps.py
+++ django_admin_keyboard_shortcuts/apps.py
@@ -1,7 +1,19 @@
 from django.apps import AppConfig
+from django.contrib import admin
 
+from django_admin_keyboard_shortcuts.sites import KSAdminSite
 
 class AdminKeyboardShortcutsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_admin_keyboard_shortcuts"
     label = "admin_keyboard_shortcuts"
+
+    def ready(self):
+        # Monkey patch the admin.site with our KSAdminSite
+        # Make sure ModelAdmin instances being registered to admin.site
+        # are registered with our KSAdminSite instead of the default one.
+        ks_admin_site = KSAdminSite()
+        def ks_admin_register(model, admin_class=None, **options):
+            ks_admin_site.register(model, admin_class, **options)
+        admin.site.register = ks_admin_register
+        admin.site = ks_admin_site
