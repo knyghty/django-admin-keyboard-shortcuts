652dcfa1babf9cad1167c993fa1d5b0f2ab19e59
khanxmetu <khanxbahria@gmail.com>
2025-04-03 15:40:02 +0300

    fix typo

--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,4 @@
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory;"}}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
